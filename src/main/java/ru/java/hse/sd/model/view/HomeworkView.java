0ef838c70398475a263f17dc4c1b9ae000a25cc3
Kate <ekaterinaicenko@gmail.com>
2022-05-23 02:31:18 +0300

    Add view comments

--- src/main/java/ru/java/hse/sd/model/view/HomeworkView.java
+++ src/main/java/ru/java/hse/sd/model/view/HomeworkView.java
@@ -1,8 +1,12 @@
 package ru.java.hse.sd.model.view;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
+/**
+ * Information about homework (for displaying).
+ **/
 public class HomeworkView {
     @JsonFormat
     private final String id;
@@ -17,8 +21,19 @@ public class HomeworkView {
     @JsonFormat
     private final String checkerId;
 
+    /**
+     * Creates new instance of HomeworkView object.
+     * Initialises id, name, publicationDate, taskDescription, deadline and checkerId with the given values.
+     *
+     * @param id              homework id
+     * @param name            homework name
+     * @param publicationDate publication date
+     * @param taskDescription task description
+     * @param deadline        homework deadline
+     * @param checkerId       checker id
+     **/
     public HomeworkView(String id, String name, LocalDateTime publicationDate,
-        String taskDescription, LocalDateTime deadline, String checkerId) {
+                        String taskDescription, LocalDateTime deadline, String checkerId) {
         this.id = id;
         this.name = name;
         this.publicationDate = publicationDate;
@@ -27,31 +42,72 @@ public class HomeworkView {
         this.checkerId = checkerId;
     }
 
+    /**
+     * Creates new instance of HomeworkView object.
+     * Initialises name, publicationDate, taskDescription, deadline and checkerId with the given values.
+     * Initialises id with null.
+     *
+     * @param name            homework name
+     * @param publicationDate publication date
+     * @param taskDescription task description
+     * @param deadline        homework deadline
+     * @param checkerId       checker id
+     **/
     public HomeworkView(String name, LocalDateTime publicationDate,
-        String taskDescription, LocalDateTime deadline, String checkerId) {
+                        String taskDescription, LocalDateTime deadline, String checkerId) {
         this(null, name, publicationDate, taskDescription, deadline, checkerId);
     }
 
+    /**
+     * Returns homework id.
+     *
+     * @return homework id
+     **/
     public String id() {
         return id;
     }
 
+    /**
+     * Returns homework name.
+     *
+     * @return homework name
+     **/
     public String name() {
         return name;
     }
 
+    /**
+     * Returns homework publication date.
+     *
+     * @return publication date
+     **/
     public LocalDateTime publicationDate() {
         return publicationDate;
     }
 
+    /**
+     * Returns task description.
+     *
+     * @return task description
+     **/
     public String taskDescription() {
         return taskDescription;
     }
 
+    /**
+     * Returns homework deadline.
+     *
+     * @return homework deadline
+     **/
     public LocalDateTime deadline() {
         return deadline;
     }
 
+    /**
+     * Returns corresponding checker id.
+     *
+     * @return checker id
+     **/
     public String checkerId() {
         return checkerId;
     }
--- src/main/java/ru/java/hse/sd/model/view/SubmissionView.java
+++ src/main/java/ru/java/hse/sd/model/view/SubmissionView.java
@@ -1,23 +1,42 @@
 package ru.java.hse.sd.model.view;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import java.io.Serializable;
 
+/**
+ * Information about submission (for displaying).
+ **/
 public class SubmissionView {
     @JsonFormat
     private final String homeworkId;
     @JsonFormat
     private final String solutionUrl;
 
+    /**
+     * Creates new instance of SubmissionView object.
+     * Initialises homeworkId and solutionUrl with the given values.
+     *
+     * @param homeworkId  homework id
+     * @param solutionUrl solution url
+     **/
     public SubmissionView(String homeworkId, String solutionUrl) {
         this.homeworkId = homeworkId;
         this.solutionUrl = solutionUrl;
     }
 
+    /**
+     * Returns homework id.
+     *
+     * @return homework id
+     **/
     public String getHomeworkId() {
         return homeworkId;
     }
 
+    /**
+     * Returns solution url.
+     *
+     * @return solution url
+     **/
     public String getSolutionUrl() {
         return solutionUrl;
     }
